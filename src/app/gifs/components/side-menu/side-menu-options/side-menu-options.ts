59cdcb227636604b2b681e33d6c1f4d0205dc845
david <tuemail@dominio.com>
2026-07-16 17:41:50 +0200

    Adding this app to GitHub

---
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
@@ -0,0 +1,40 @@
+import {Component, computed, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+import { GifsService } from 'src/app/gifs/services/gifs.service';
+
+interface MenuOption {
+  label: string;
+  subLabel: string;
+  route: string;
+  icon: string;
+}
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './side-menu-options.html',
+})
+export class SideMenuOptions {
+
+
+  gifService = inject(GifsService);
+
+
+
+  menuOptions:MenuOption[] = [
+
+    {
+    icon: 'fa-solid fa-chart-line',
+    label: 'Trending',
+    subLabel: 'Gifs Populares',
+    route: '/dashboard/trending',
+    },
+    {
+    icon: 'fa-solid fa-magnifying-glass',
+    label: 'Buscador',
+    subLabel: 'Buscar gifs',
+    route: '/dashboard/search',
+    },
+  ]
+
+}
---
+++ src/app/gifs/pages/search-page/search-page.ts
@@ -0,0 +1,23 @@
+import { Component, inject, signal } from '@angular/core';
+import { GifList } from "../../components/gif-list/gif-list";
+import { GifsService } from '../../services/gifs.service';
+import { Gif } from '../../interfaces/gif.interface';
+
+@Component({
+  selector: 'app-search-page',
+  imports: [GifList],
+  templateUrl: './search-page.html',
+})
+export default class SearchPage {
+
+  gifService = inject(GifsService);
+  gifs = signal<Gif[]>([]);
+
+  onSearch(query:string) {
+    this.gifService.searchGifs(query)
+    .subscribe(resp => {
+      this.gifs.set(resp);
+    });
+  }
+
+}
